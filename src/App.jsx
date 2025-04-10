f1436b2e0bedd7a5e549e73461edac4de0c008e5
netherhouse <nether3ord@gmail.com>
2025-04-11 00:40:03 +0200

    Implement TaskForm component and update App to include task management structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+function TaskForm() {
+  return (
+    <form action="" className="task-form">
+      <input type="text" value={""} placeholder="task title" required />
+      <select value="">
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <input type="datetime-local" required value="" />
+      <button type="submit">add Task</button>
+    </form>
+  );
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="task-container">
+        <h1>Task list with Priority</h1>
+        <button className="close-button">+</button>
+        <TaskForm />
+      </div>
+      <div className="task-container">
+        <h2>Tasks</h2>
+        <button className="close-button">+</button>
+      </div>
+      <div className="completed-task-container">
+        <h2>Completed Task</h2>
+        <button className="close-button">+</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
